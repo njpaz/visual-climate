cb3328052883491fd5de5a412bd57f95f619e6c1
Nicole <nicole.j.paz@gmail.com>
2016-06-01 19:52:27 -0700

    Separate chart init from chart populate.

--- app/components/visual-chart.js
+++ app/components/visual-chart.js
@@ -5,10 +5,30 @@ export default Ember.Component.extend({
   tagName: 'svg',
   width: 1000,
   height: 500,
+  margins: {top: 20, right: 20, bottom: 20, left: 50},
+  chart: null,
+  chartX: null,
+  chartY: null,
+  chartLine: null,
+  chartTip: null,
 
   didInsertElement() {
-    this._insertChart();
+    this._initChart();
+    this._setChartAxes();
+    this._populateChart();
   },
+
+  resetChartOnDataChange: Ember.observer('chartData.@each.data', function() {
+    let chart = this.get('chart');
+
+    chart.selectAll('g.axis').remove();
+    chart.selectAll('path').remove();
+    chart.selectAll('circle').remove();
+
+    this._initChart();
+    this._setChartAxes();
+    this._populateChart();
+  }),
 
   chartDates: Ember.computed('chartData.@each.data', function() {
     let chartData = this.get('chartData');
@@ -30,22 +50,49 @@ export default Ember.Component.extend({
       return reducedValues;
     }, []);
   }),
+  formatTime: Ember.computed(function() {
+    return d3.time.format('%b %Y');
+  }),
 
-  _insertChart() {
-    const margin = {top: 20, right: 20, bottom: 20, left: 50};
+  _initChart() {
+    let margins = this.get('margins');
     let width = this.get('width');
     let height = this.get('height');
     let chartData = this.get('chartData');
+
+    let tip = d3.select('body').append('div')
+        .attr('class', 'tooltip')
+        .style('opacity', 0);
+
+    let chart = d3.select(`#${this.get('elementId')}`)
+        .attr('width', width + margins.left + margins.right)
+        .attr('height', height + margins.top + margins.bottom)
+      .append('g')
+        .attr('transform', `translate(${margins.left},${margins.top})`);
+
+    this.set('chart', chart);
+    this.set('chartTip', tip);
+  },
+  _setChartAxes() {
+    let chart = this.get('chart');
     let chartDates = this.get('chartDates');
     let chartValues = this.get('chartValues');
+    let margins = this.get('margins');
+    let width = this.get('width');
+    let height = this.get('height');
 
     let x = d3.time.scale()
         .domain([d3.min(chartDates), d3.max(chartDates)])
-        .range([margin.left, width - margin.right]);
+        .range([margins.left, width - margins.right]);
 
     let y = d3.scale.linear()
         .domain([d3.min(chartValues), d3.max(chartValues)])
-        .range([height - margin.top, margin.bottom]);
+        .range([height - margins.top, margins.bottom]);
+
+    let line = d3.svg.line()
+        .x(function(d) { return x(d.date); })
+        .y(function(d) { return y(d.value); })
+        .interpolate('basis');
 
     let xAxis = d3.svg.axis()
         .scale(x)
@@ -56,12 +103,6 @@ export default Ember.Component.extend({
         .scale(y)
         .orient('left');
 
-    let chart = d3.select(`#${this.get('elementId')}`)
-        .attr('width', width + margin.left + margin.right)
-        .attr('height', height + margin.top + margin.bottom)
-      .append('g')
-        .attr('transform', `translate(${margin.left},${margin.top})`);
-
     chart.append('g')
         .attr('class', 'x axis')
         .attr('transform', `translate(0,${height})`)
@@ -69,19 +110,21 @@ export default Ember.Component.extend({
 
     chart.append('g')
         .attr('class', 'y axis')
-        .attr('transform', `translate(${margin.left},0)`)
+        .attr('transform', `translate(${margins.left},0)`)
         .call(yAxis);
 
-    let line = d3.svg.line()
-        .x(function(d) { return x(d.date); })
-        .y(function(d) { return y(d.value); })
-        .interpolate('basis');
-
-    let tip = d3.select('body').append('div')
-        .attr('class', 'tooltip')
-        .style('opacity', 0);
-
-    let formatTime = d3.time.format('%b %Y');
+    this.set('chartY', y);
+    this.set('chartX', x);
+    this.set('chartLine', line);
+  },
+  _populateChart() {
+    let chart = this.get('chart');
+    let chartData = this.get('chartData');
+    let formatTime = this.get('formatTime');
+    let line = this.get('chartLine');
+    let tip = this.get('chartTip');
+    let x = this.get('chartX');
+    let y = this.get('chartY');
 
     chartData.forEach(function(ad) {
       chart.append('path')
